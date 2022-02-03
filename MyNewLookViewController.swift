947ea73ab5ce8dc41ef1bd4a4f58d9e2cba56d6c
minjiLee68 <dlalswl2598@gmail.com>
2022-02-03 22:51:17 +0900

    .

---
+++ MyNewLookViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyNewLookViewController.swift
+//  MyNewLook
+//
+//  Created by 이민지 on 2022/02/03.
+//
+
+import UIKit
+
+class MyNewLookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
