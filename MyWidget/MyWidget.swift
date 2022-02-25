4b5e01ef407c8f0bd4dc069f2eb9bae52574e151
minjiLee68 <dlalswl2598@gmail.com>
2022-02-26 00:15:53 +0900

    .

---
+++ MyWidget/MyWidget.swift
@@ -0,0 +1,144 @@
+//
+//  MyWidget.swift
+//  MyWidget
+//
+//  Created by 이민지 on 2022/02/24.
+//
+
+import WidgetKit
+import SwiftUI
+import Foundation
+import RealmSwift
+import FSCalendar
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct MyWidgetEntryView : View {
+    @Environment(\.widgetFamily) var family
+    @Environment(\.colorScheme) var scheme
+    var entry: Provider.Entry
+
+    var body: some View {
+        ZStack {
+            Color("WidgetBackground").frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    header
+                    Spacer()
+                    pricting
+                    Spacer()
+                }.padding(20)
+                
+                Spacer()
+            }
+        }
+    }
+    
+    var header: some View {
+        Group {
+            Text("WAY APP")
+                .bold()
+                .font(family == .systemLarge ? .system(size: 40) : .title)
+                .minimumScaleFactor(0.5)
+            Text("look!!")
+                .font(family == .systemLarge ? .title : .headline)
+        }
+        .foregroundColor(Color("titleColor"))
+    }
+    
+    var pricting: some View {
+        Group {
+            
+        }
+    }
+    
+//    var price: some View {
+//        Text (entry.error ? "+ ----" : "\(entry.date.diffMode == .up ? "+" : "")")
+//    }
+}
+
+@main
+struct MyWidget: Widget {
+    let kind: String = "MyWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            MyWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("WAY Widget")
+        .description("This is an example widget.")
+        .supportedFamilies([.systemMedium, .systemLarge])
+    }
+}
+
+struct MyWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        MyWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+            .environment(\.colorScheme, .light)
+//            .redacted(reason: .placeholder)
+        MyWidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemLarge))
+            .environment(\.colorScheme, .light)
+//            .redacted(reason: .placeholder)
+    }
+}
+
+//struct CalendarRepresentable {
+//    typealias Body = FSCalendar
+//
+//    var calendar = FSCalendar()
+//    var selectedDate: Date
+//
+//    func makeUIView(context: Context) -> FSCalendar {
+//        calendar.delegate = context.coordinator
+//        calendar.dataSource = context.coordinator
+//
+//
+//        return calendar
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+//    class Coordinator: NSObject, FSCalendarDelegate, FSCalendarDataSource {
+//        var parent: CalendarRepresentable
+//        init(_ parent: CalendarRepresentable) {
+//            self.parent = parent
+//        }
+//
+//
+//    }
+//
+//    func updateUIView(_ uiView: FSCalendar, context: Context) {}
+//}
